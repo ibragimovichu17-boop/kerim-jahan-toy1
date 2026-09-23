b15cd6943f12c280d60ff7877a2c637b88659cc1
ibragimovichu17-boop <ibragimovich.u17@gmail.com>
2026-09-23 19:07:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-// Aýdym faýlyny we başlanýan sekundyny kesgitleýäris
-const audio = new Audio('copy_1D78BEEC-3576-4A52-BC73-736F7E45F238.mp3');
-audio.loop = true;
+overlay.addEventListener('click', () => {
+  if (isOpened) return;
+  isOpened = true;
 
-let isPlayedOnce = false;
-
-// Sahypadaky ilkinji basyşda aýdymy 11-nji sekuntdan başlatmak
-document.addEventListener('click', function() {
-  if (!isPlayedOnce && audio.paused) {
+  if (audio.paused) {
+    // Aýdymy 11-nji sekunda geçirýäris
     audio.currentTime = 11;
     audio.play().then(() => {
-      isPlayedOnce = true;
-    }).catch(err => {
-      console.log("Autoplay çäklendirildi:", err);
-    });
+      musicIcon.classList.remove("fa-music");
+      musicIcon.classList.add("fa-pause");
+    }).catch(e => console.log("Audio play error:", e));
   }
-}, { once: true });
+
+  overlay.classList.add('opened');
+
+  setTimeout(() => {
+    AOS.refresh();
+  }, 600);
+});
+
+musicBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (audio.paused) {
+    isOpened = true;
+    // Eger aýdym täze başlanýan bolsa 11-nji sekuntdan başlatsyn
+    if (audio.currentTime < 11) {
+      audio.currentTime = 11;
+    }
+    audio.play();
+    musicIcon.classList.remove("fa-music");
+    musicIcon.classList.add("fa-pause");
+  } else {
+    audio.pause();
+    musicIcon.classList.remove("fa-pause");
+    musicIcon.classList.add("fa-music");
+  }
+});
